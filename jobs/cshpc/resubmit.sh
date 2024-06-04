99f39cc11684a20b42a7064f1b3c20cdaa02b50b
MVCowley <51127523+MVCowley@users.noreply.github.com>
2024-06-04 11:37:06 +0100

    Update resubmit.sh to handle >1 .tsv expectation

--- jobs/cshpc/resubmit.sh
+++ jobs/cshpc/resubmit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+read -p "Enter the number of TSV files expected per folder: " EXPECTED_COUNT
+
 STARTDIR=$(pwd)
 TEMPDIR=temp
 # Rember to change job script based on species and protocol
@@ -8,14 +10,17 @@ JOB=dcr_job.qsub.sh
 for i in $TEMPDIR/*
 do
   cd $i/
-  FILENAME=$(find . -type f -name "*.tsv*" -exec basename {} \;)
-  if [ -z "${FILENAME}" ]
+  # Count the number of TSV files in the folder
+  TSV_COUNT=$(find . -type f -name "*.tsv*" | wc -l)
+  
+  if [ $TSV_COUNT -ne $EXPECTED_COUNT ]
   then
     echo $i
-    echo "No tsv found. Re-running with new job script."
+    echo "Expected $EXPECTED_COUNT TSV files, but found $TSV_COUNT. Re-running with new job script."
     cp $STARTDIR/$JOB .
     qsub $JOB
   fi
+  
   cd $STARTDIR
 done
 
