0b046b6108aebfa07aa8e6b02f59e5507f31077e
MVCowley <51127523+MVCowley@users.noreply.github.com>
2024-08-27 14:26:46 +0100

    feat(cshpc): make summarise.sh target most recent logs

--- jobs/cshpc/summarise.sh
+++ jobs/cshpc/summarise.sh
@@ -3,6 +3,7 @@
 
 PROJECTDIR=/SAN/colcc/tcr_decombinator
 TOOLS=$PROJECTDIR/Decombinator-Tools
+TARGET_DIR=temp/
 
 (return 0 2>/dev/null) && sourced=1 || sourced=0
 
@@ -23,18 +24,30 @@ fi
 # Create temp directory for logs
 echo "Creating temporary directory..."
 mkdir temp_logs
+DEST_DIR=temp_logs/
 
-echo "Copying logs..."
-find temp/ -type f -name "*.csv*" -exec cp {} temp_logs/ \;
+find $TARGET_DIR -type f -name "*.csv" | awk -v dest_dir="$DEST_DIR" -F'[_.]' '{
+    date = $1 "_" $2 "_" $3;
+    raw_id = substr($0, index($0, $4))
+    if (!seen[raw_id] || date > seen[raw_id]) {
+        seen[raw_id] = date;
+        files[raw_id] = $0;
+    }
+} END {
+    for (id in files) {
+        print "Copying " files[id] " to " dest_dir;
+        system("cp \"" files[id] "\" \"" dest_dir "\"");
+    }
+}'
 
 # Generate summary sheet
 echo "Creating temporary summary sheet..."
-source /share/apps/source_files/python/python-3.10.0.source
+source /share/apps/source_files/python/python-3.11.9.source
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
 python3 $TOOLS/analysis/LogSummary.py -l temp_logs/ -o Summary_${POOLID}_${current_time}.csv -s $POOLID.csv
 
 # Remove temp logs
 echo "Removing temporary directory..."
-rm -r temp_logs
+rm -r temp_logs/
 
 echo "Summary sheet generated."
