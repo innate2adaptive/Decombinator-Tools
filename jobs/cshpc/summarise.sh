fc1a374018085f9baa672d1a49e20133fe739f74
MVCowley <51127523+MVCowley@users.noreply.github.com>
2024-10-15 09:27:55 +0100

    feat(summarise): filter identical dates

--- jobs/cshpc/summarise.sh
+++ jobs/cshpc/summarise.sh
@@ -9,10 +9,10 @@ TARGET_DIR=temp/
 
 read -ep "Please enter the pool ID: " POOLID
 
-if [ -f $POOLID.csv ]; then
-    echo "$POOLID.csv exists, proceeding with summarising."
+if [ -f $POOLID ]; then
+    echo "$POOLID exists, proceeding with summarising."
 else
-    echo "$POOLID.csv does not exist, please add it to the directory."
+    echo "$POOLID does not exist, please add it to the directory."
     echo 'Exiting.'
     if [ $sourced -eq 1 ]; then
         return
@@ -27,24 +27,46 @@ mkdir temp_logs
 DEST_DIR=temp_logs/
 
 find $TARGET_DIR -type f -name "*.csv" | awk -v dest_dir="$DEST_DIR" -F'[_.]' '{
-    date = $1 "_" $2 "_" $3;
-    raw_id = substr($0, index($0, $4))
-    if (!seen[raw_id] || date > seen[raw_id]) {
-        seen[raw_id] = date;
-        files[raw_id] = $0;
-    }
+	n = split($0, parts, "/");
+	filename = parts[n];
+	split(filename, name_parts, "_");
+	date = name_parts[1] "_" name_parts[2] "_" name_parts[3];
+	numeric_date = name_parts[1] name_parts[2] name_parts[3];
+	no_date_name = substr(filename, length(date) + 2);
+	sub(/\.csv$/, "", filename);
+	match(filename, /[0-9]*$/);
+
+	print filename
+
+	if (RSTART > 0) {
+		seq_num = substr(filename, RSTART);
+		raw_id = substr(no_date_name, 1, RSTART - 1);
+	} else {
+		seq_num = 0;
+		raw_id = no_date_name;
+	}
+
+	id = gensub(/_+$/, "", "g", raw_id);
+	
+	if (!seen[id] || numeric_date > seen_date[id] || (numeric_date == seen_date[id] && seq_num > seen_seq[id])) {
+		seen_date[id] = numeric_date;
+		seen_seq[id] = seq_num;
+		files[id] = $0;
+		print files[id] "replaces" files[id];
+	}
+
 } END {
-    for (id in files) {
-        print "Copying " files[id] " to " dest_dir;
-        system("cp \"" files[id] "\" \"" dest_dir "\"");
-    }
+	for (id in files) {
+		print "Copying " files[id] " to " dest_dir;
+		system("cp \"" files[id] "\" \"" dest_dir "\"");
+	}
 }'
 
 # Generate summary sheet
 echo "Creating temporary summary sheet..."
 source /share/apps/source_files/python/python-3.11.9.source
 current_time=$(date +"%Y-%m-%d_%H-%M-%S")
-python3 $TOOLS/analysis/LogSummary.py -l temp_logs/ -o Summary_${POOLID}_${current_time}.csv -s $POOLID.csv
+python3 $TOOLS/analysis/LogSummary.py -l temp_logs/ -o Summary_${current_time}_${POOLID} -s $POOLID
 
 # Remove temp logs
 echo "Removing temporary directory..."
