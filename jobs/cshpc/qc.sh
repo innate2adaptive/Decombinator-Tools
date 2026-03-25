c44856d64980f197c1bdbd17d185f9af4a26ca04
MVCowley <51127523+MVCowley@users.noreply.github.com>
2026-03-25 15:24:26 +0000

    feat(qc): make qc script have defualt path

--- jobs/cshpc/qc.sh
+++ jobs/cshpc/qc.sh
@@ -6,6 +6,7 @@ TOOLS=$PROJECTDIR/Decombinator-Tools
 source /share/apps/source_files/python/python-3.11.9.source
 source $PROJECTDIR/decombinator_qc_venv/bin/activate
 
-read -p "Please enter the glob path to the .tsv files to be qc'd: " TSV_GLOB
+read -p "Please enter the glob path to the .tsv files to be qc'd [temp/*/*.tsv.gz]: " TSV_GLOB
+TSV_GLOB=${TSV_GLOB:-temp/*/*.tsv.gz}
 
 python3 $TOOLS/analysis/qc.py $TSV_GLOB -o qc_plots
